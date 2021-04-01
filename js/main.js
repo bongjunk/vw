445ce82edada474b22ef939812655c071afa53ec
bongjun-kim94 <74786253+bongjun-kim94@users.noreply.github.com>
2021-04-01 11:44:19 +0900

    vw

---
+++ js/main.js
@@ -0,0 +1,68 @@
+$(function(){
+// 메인페이지 fade in, fade out
+    var fadeN = 0;
+
+    $('.main_01 .imgList img').not(':first').hide();
+    setInterval(function(){
+        $('.main_01 .imgList img').eq(fadeN).fadeOut(5000);
+        fadeN++;
+        if(fadeN == 4){fadeN = 0}
+        $('.main_01 .imgList img').eq(fadeN).fadeIn(5000);
+
+
+
+        var imgSrc = $('.main_01 .imgList img').eq(fadeN).attr('src');
+        var imgB = $('.play .cell a').index();
+
+        console.log(imgSrc);
+        console.log(imgB);
+
+        $('.play .cell a').on('click', function(){
+            
+            
+            $('.main_01 .imgList img').attr('src',imgSrc)
+    
+            // $('.main_01 .imgList img').attr('fadeN',imgSrc)
+        });
+
+    },5000);
+    
+
+
+    $(document).ready(function() {
+
+        //좌우버튼, 동그라미 버튼 연동 //
+      
+        var i = $(".btn li.on").index();
+        var len = $(".panel li").length;
+    
+        $(".btn li").click(function() {
+          i = $(this).index();
+          showSlide();
+        });
+        function showSlide() {
+          $(".btn li").removeClass("on");
+          $(".btn li").eq(i).addClass("on");
+          $(".panel li").stop(true, true).fadeOut();
+          $(".panel li").eq(i).stop(true, true).fadeIn();
+        }
+      });
+
+    // function imgBtn(){
+   
+    // }
+    // imgBtn();
+
+    // $('.play .cell a').on('click',function(){
+        // $('.main_01 .imgList img').not(':first').hide();
+        // setInterval(function(){
+        //     $('.main_01 .imgList img').eq(fadeN).fadeOut(2000);
+        //     fadeN++;
+        //     if(fadeN == 4){fadeN = 0}
+        //     $('.main_01 .imgList img').eq(fadeN).fadeIn(2000);
+        // },2000);
+        // console.log(fadeN);
+
+    //});
+
+});
