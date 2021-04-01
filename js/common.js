5508a89f0c986a70255d455fd0e584bb1e97550d
bongjun-kim94 <74786253+bongjun-kim94@users.noreply.github.com>
2021-04-01 11:33:52 +0900

    vw

--- js/common.js
+++ js/common.js
@@ -1,32 +1,56 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
-/******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
+$(function(){
 
-/***/ "./src/js/common.js":
-/*!**************************!*\
-  !*** ./src/js/common.js ***!
-  \**************************/
-/***/ (() => {
+    $('.header').load('inc.html header div', init);
 
-eval("$(function () {\n  $('.header').load('inc.html header div', init);\n\n  function init() {\n    $('nav a').on('click', function () {\n      localStorage.pageNum = $(this).index();\n    });\n    $('nav a').eq(localStorage.pageNum).css({\n      opacity: 0.5\n    });\n  }\n\n  function mainScroll() {\n    var delta,\n        loop,\n        num = 0,\n        pos = [];\n    $('.scroll').each(function (i) {\n      pos.push($('.scroll').eq(i).offset().top);\n    });\n    console.log(pos);\n    $(window).on('mousewheel DOMMouseScroll', function (e) {\n      delta = e.originalEvent.wheelDelta || -e.originalEvent.wheelDelta;\n      clearTimeout(loop);\n      loop = setTimeout(function () {\n        if (delta < 0) {\n          // down\n          //$(window).scrollTop(1000);\n          if (num != 7) num++;\n        } else {\n          // up\n          //$(window).scrollTop(0);\n          if (num != 0) num--;\n        }\n\n        $('main > div').animate({\n          marginTop: -pos[num]\n        }); //$('main > div').animate({marginTop:$(window).height() * -num})\n      }, 100);\n    });\n  }\n\n  mainScroll();\n});\n\n//# sourceURL=webpack://project_02/./src/js/common.js?");
+    function init(){
+        $('nav a').on('click',function(){
+            localStorage.pageNum = $(this).index();
+        });
 
-/***/ })
+        $('nav a').eq(localStorage.pageNum).css({ opacity:0.5 });
+    }
 
-/******/ 	});
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/js/common.js"]();
-/******/ 	
-/******/ })()
-;
+
+    function mainScroll(){
+        var delta, loop, num = 0, pos = [];
+    
+        $('.scroll').each(function (i) {
+            pos.push($('.scroll').eq(i).offset().top); 
+        });
+
+        console.log(pos);
+        $(window).on('mousewheel DOMMouseScroll', function (e) {
+            delta = e.originalEvent.wheelDelta || -e.originalEvent.wheelDelta;
+    
+            clearTimeout(loop);
+            loop = setTimeout(function () {
+                if (delta < 0) {
+                    // down
+                    //$(window).scrollTop(1000);
+                    if (num != 7) num++;
+                } else {
+                    // up
+                    //$(window).scrollTop(0);
+                    if (num != 0) num--;
+                }
+                $('main > div').animate({ marginTop: -pos[num]});
+                //$('main > div').animate({marginTop:$(window).height() * -num})
+            }, 100)
+        });
+    }
+    mainScroll();
+
+    function scrollImg(){
+        // 모바일 상태 top버튼
+        $(window).scroll(function() {
+          if ($(this).scrollTop() > 1700) {
+              $('.main_06 img').fadeIn();
+          } else {
+              $('.main_06 img').fadeOut();
+          }
+        });
+    
+    }
+    scrollImg();
+});
+
--- js/main.js
+++ js/main.js
@@ -1,32 +1,68 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
-/******/ (() => { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ({
+$(function(){
+// 메인페이지 fade in, fade out
+    var fadeN = 0;
 
-/***/ "./src/js/main.js":
-/*!************************!*\
-  !*** ./src/js/main.js ***!
-  \************************/
-/***/ (() => {
+    $('.main_01 .imgList img').not(':first').hide();
+    setInterval(function(){
+        $('.main_01 .imgList img').eq(fadeN).fadeOut(5000);
+        fadeN++;
+        if(fadeN == 4){fadeN = 0}
+        $('.main_01 .imgList img').eq(fadeN).fadeIn(5000);
 
-eval("$(function () {\n  // 메인페이지 fade in, fade out\n  var fadeN = 0;\n  $('.main_01 .imgList img').not(':first').hide();\n  setInterval(function () {\n    $('.main_01 .imgList img').eq(fadeN).fadeOut(5000);\n    fadeN++;\n\n    if (fadeN == 4) {\n      fadeN = 0;\n    }\n\n    $('.main_01 .imgList img').eq(fadeN).fadeIn(5000);\n    var imgSrc = $('.main_01 .imgList img').eq(fadeN).attr('src');\n    var imgB = $('.play .cell a').index();\n    console.log(imgSrc);\n    console.log(imgB);\n    $('.play .cell a').on('click', function () {\n      $('.main_01 .imgList img').attr('src', imgSrc); // $('.main_01 .imgList img').attr('fadeN',imgSrc)\n    });\n  }, 5000);\n  $(document).ready(function () {\n    //좌우버튼, 동그라미 버튼 연동 //\n    var i = $(\".btn li.on\").index();\n    var len = $(\".panel li\").length;\n    $(\".btn li\").click(function () {\n      i = $(this).index();\n      showSlide();\n    });\n\n    function showSlide() {\n      $(\".btn li\").removeClass(\"on\");\n      $(\".btn li\").eq(i).addClass(\"on\");\n      $(\".panel li\").stop(true, true).fadeOut();\n      $(\".panel li\").eq(i).stop(true, true).fadeIn();\n    }\n  }); // function imgBtn(){\n  // }\n  // imgBtn();\n  // $('.play .cell a').on('click',function(){\n  // $('.main_01 .imgList img').not(':first').hide();\n  // setInterval(function(){\n  //     $('.main_01 .imgList img').eq(fadeN).fadeOut(2000);\n  //     fadeN++;\n  //     if(fadeN == 4){fadeN = 0}\n  //     $('.main_01 .imgList img').eq(fadeN).fadeIn(2000);\n  // },2000);\n  // console.log(fadeN);\n  //});\n});\n\n//# sourceURL=webpack://project_02/./src/js/main.js?");
 
-/***/ })
 
-/******/ 	});
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/js/main.js"]();
-/******/ 	
-/******/ })()
-;
+        var imgSrc = $('.main_01 .imgList img').eq(fadeN).attr('src');
+        var imgB = $('.play .cell a').index();
+
+        console.log(imgSrc);
+        console.log(imgB);
+
+        $('.play .cell a').on('click', function(){
+            
+            
+            $('.main_01 .imgList img').attr('src',imgSrc)
+    
+            // $('.main_01 .imgList img').attr('fadeN',imgSrc)
+        });
+
+    },5000);
+    
+
+
+    $(document).ready(function() {
+
+        //좌우버튼, 동그라미 버튼 연동 //
+      
+        var i = $(".btn li.on").index();
+        var len = $(".panel li").length;
+    
+        $(".btn li").click(function() {
+          i = $(this).index();
+          showSlide();
+        });
+        function showSlide() {
+          $(".btn li").removeClass("on");
+          $(".btn li").eq(i).addClass("on");
+          $(".panel li").stop(true, true).fadeOut();
+          $(".panel li").eq(i).stop(true, true).fadeIn();
+        }
+      });
+
+    // function imgBtn(){
+   
+    // }
+    // imgBtn();
+
+    // $('.play .cell a').on('click',function(){
+        // $('.main_01 .imgList img').not(':first').hide();
+        // setInterval(function(){
+        //     $('.main_01 .imgList img').eq(fadeN).fadeOut(2000);
+        //     fadeN++;
+        //     if(fadeN == 4){fadeN = 0}
+        //     $('.main_01 .imgList img').eq(fadeN).fadeIn(2000);
+        // },2000);
+        // console.log(fadeN);
+
+    //});
+
+});
